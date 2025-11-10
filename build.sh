f8edff78ea8767fb0ed32f0ed439909433664139
hjembrek48 <hj54652@fer.hr>
2025-11-10 16:00:29 +0000

    ovaj put stvarno spremno za render drugi put

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies
-pip install -r requirements.txt
+cd backend
+pip install -r ../requirements.txt
 
 python manage.py collectstatic --no-input
-
 python manage.py migrate
