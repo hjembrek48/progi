fe2340ab428df7bd0c24da1359ad998a5f204837
hjembrek48 <hj54652@fer.hr>
2026-01-12 11:07:20 +0000

    ogroman bijes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,8 +40,9 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", os.getenv("RENDER_EXTERNAL_HOSTNAME",
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
+        #"rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        #"rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
